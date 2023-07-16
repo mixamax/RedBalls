00b8d1c7ba3bff665bfc3fd26189a28323c19bf7
Mixamax <mixamax@inbox.ru>
2023-07-16 16:55:33 +0300

    rename .html

--- balls.js
+++ balls.js
@@ -109,3 +109,5 @@
     window.addEventListener("mousedown", isDown);
     window.addEventListener("mouseup", isDown);
 })();
+
+// <h1 align="center">This code is taken to study canvas from a wonderful channel  <a href="https://youtu.be/KEQsm2yL6Lg" target="_blank">ThreePixDroid</a>
